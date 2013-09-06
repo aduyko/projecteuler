c3161f931c1c2de320278238586e4e0b1beeb60e
Andriy Duyko <andriy@aduyko.com>
2013-09-06 19:13:58 -0400

    added problem 5, renamed files from px to p00x

--- p2-evenfib.rb
+++
@@ -1,8 +0,0 @@
-first=1
-second=1
-evensum=0
-until second>4000000
-	if second%2==0 ; evensum+=second end
-	first,second=second,first+second
-end
-print evensum
--- p4-palnum.rb
+++
@@ -1,8 +0,0 @@
-palindrome=0
-999.downto(100) { |i|
-	i.downto(100) { |j|
-		result=(i*j).to_s
-		if result==result.reverse && Integer(palindrome)<Integer(result) ; palindrome = result	end
-	}
-}
-print palindrome
