ae6cf4e924c921e17845fae72a8d06b5d9b2915b
Andriy Duyko <andriy@aduyko.com>
2013-09-14 01:38:24 -0400

    problem 30 guessing upper bound
    recommit to remove some testing stuff, still no idea how to
    properly find upper bound

--- p030-digitfifthpowers.rb
+++ p030-digitfifthpowers.rb
@@ -3,7 +3,4 @@ nums=[]
   digits=i.to_s.split("").map { |i| i.to_i**5 } .inject(:+)
   if digits==i ; nums.push(i) end
 }
-nums.each { |i|
-  puts "#{i}: #{i.to_s.split("").map { |j| j.to_i**5 }}"
-}
 print nums.inject(:+)
