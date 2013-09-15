ef6c3c79813ea86840c7c0b7a6463c4e0712b12c
Andriy Duyko <andriy@aduyko.com>
2013-09-15 13:02:08 -0400

    problem 33

---
+++ p033-digitcancelfractions.rb
@@ -0,0 +1,27 @@
+max=100
+min=10
+product=1
+(min...max).each { |num|
+  if num%10==0 ; next end
+  num_first = num/10.floor
+  num_second = num%10
+  (num+1..max).each { |denom|
+    if denom%10==0 ; next end                                 #division by 0
+    simplified=Rational(num,denom)
+    if simplified.denominator==denom ; next end
+    denom_first = denom/10.floor
+    denom_second = denom%10
+    if   num_first==num_second ||
+         denom_first==denom_second ||
+         (num_first!=denom_second &&
+         denom_first!=num_second)
+      next
+    end
+    if   simplified==Rational(num_first,denom_second) ||
+         simplified==Rational(num_second,denom_first) 
+      puts "#{num}/#{denom} : #{simplified}"
+      product*=simplified
+    end
+  }
+}
+puts product.denominator
