468c3b4163085890c3c836acf6ccb786858c3768
Andriy Duyko <andriy@aduyko.com>
2013-09-25 13:11:41 -0400

    problem 46

--- p046-goldbachotherconj.rb
+++ p046-goldbachotherconj.rb
@@ -1,9 +1,26 @@
 require_relative 'equations'
 
-start = 144
-hex = getHexagonal(start)
-until isPentagonal? hex 
-  start+=1
-  hex = getHexagonal(start)
+$squares = []
+$last = 20
+(1..$last).each { |i|
+  $squares.push(2 * (i**2))
+}
+def equation(n)
+  if n>$squares.last
+    $last+=1
+    $squares.push(2 * ($last**2))
+  end
+  $squares.each { |i|
+    if i>n ; return false end
+    if isPrime(n-i) ; return true end
+  }
+  return false
 end
-puts hex
+test = 9
+while equation(test)
+  while isPrime(test+2)
+    test+=2
+  end
+  test+=2
+end
+puts test
